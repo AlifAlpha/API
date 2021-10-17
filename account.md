a4c798562bb787f658c46e6917de36cc9880164a
SohaibArbiBakcha <bakchalona@gmail.com>
2021-10-17 13:53:38 +0100

    first commit

---
+++ account.md
@@ -0,0 +1,11 @@
+##### heroku
+
+## email : bakchalona@gmail.com
+
+## pw : wcmhk9G&
+
+##### mongodb
+
+## email : kalyan.jivan@zooants.com
+
+## pw : wcmhk9G&
---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const morgen = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Range"],
+    exposedHeaders: ["Content-Range", "X-Content-Range"],
+    credentials: true,
+  })
+);
+
+dotenv.config();
+
+// db config
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MongoDB successfully connected");
+  });
+
+mongoose.connection.on("error", (err) => {
+  console.log(`db connection error  : ${err.message}`);
+});
+// middelware
+app.use(morgen("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//bring routes
+// const authRouter = require("./routes/auth");
+
+// app.use("/", authRouter);
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`node api lisnting to port : ${port}`);
+});
