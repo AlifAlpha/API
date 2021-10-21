825a93530f87a8b86dd792eab302d0f69f83b570
SohaibArbiBakcha <bakchalona@gmail.com>
2021-10-21 12:39:18 +0100

    make it better

--- routers/auth.js
+++ routers/auth.js
@@ -4,6 +4,7 @@ const { signin, signout } = require("../controllers/auth");
 const router = express.Router();
 
 router.post("/auth", signin);
+//llll
 router.get("/auth", signout);
 
 module.exports = router;
