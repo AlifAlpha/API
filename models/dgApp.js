33f1f8c0646342f3c6e633223215f201d21fc592
SohaibArbiBakcha <bakchalona@gmail.com>
2021-10-23 17:02:00 +0100

    test to step back

--- models/dgApp.js
+++ models/dgApp.js
@@ -1,59 +1,59 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const DgAppSchema = new mongoose.Schema({
-  // appType: {
-  //   // required: true,
-  //   inPerson: {
-  //     meeting: { type: Boolean, default: false },
-  //     visit: { type: Boolean, default: false },
-  //     sign: { type: Boolean, default: false },
-  //   },
-  //   virtual: {
-  //     visit: { type: Boolean, default: false },
-  //     sign: { type: Boolean, default: false },
-  //   },
-  // },
-  appType: {
-    required: true,
-    type: String,
-    trim: true,
-  },
-  startMeet: {
-    required: true,
-    type: Date,
-  },
-  timeMeet: {
-    required: true,
-    type: String,
-  },
-  name: { required: true, type: String, trim: true },
-  title: { required: true, type: String, trim: true },
-  dateDurStart: {
-    required: true,
-    type: Date,
-  },
-  dateDurEnd: {
-    type: Date,
-  },
-  timeDurStart: {
-    required: true,
-    type: String,
-  },
-  timeDurEnd: {
-    required: true,
-    type: String,
-  },
-  purpose: { required: true, type: String, trim: true },
-});
+// const DgAppSchema = new mongoose.Schema({
+//   // appType: {
+//   //   // required: true,
+//   //   inPerson: {
+//   //     meeting: { type: Boolean, default: false },
+//   //     visit: { type: Boolean, default: false },
+//   //     sign: { type: Boolean, default: false },
+//   //   },
+//   //   virtual: {
+//   //     visit: { type: Boolean, default: false },
+//   //     sign: { type: Boolean, default: false },
+//   //   },
+//   // },
+//   appType: {
+//     required: true,
+//     type: String,
+//     trim: true,
+//   },
+//   startMeet: {
+//     required: true,
+//     type: Date,
+//   },
+//   timeMeet: {
+//     required: true,
+//     type: String,
+//   },
+//   name: { required: true, type: String, trim: true },
+//   title: { required: true, type: String, trim: true },
+//   dateDurStart: {
+//     required: true,
+//     type: Date,
+//   },
+//   dateDurEnd: {
+//     type: Date,
+//   },
+//   timeDurStart: {
+//     required: true,
+//     type: String,
+//   },
+//   timeDurEnd: {
+//     required: true,
+//     type: String,
+//   },
+//   purpose: { required: true, type: String, trim: true },
+// });
 
-DgAppSchema.method("transform", function () {
-  let obj = this.toObject();
+// DgAppSchema.method("transform", function () {
+//   let obj = this.toObject();
 
-  //id renaming
+//   //id renaming
 
-  obj.id = obj._id;
-  delete obj._id;
-  return obj;
-});
+//   obj.id = obj._id;
+//   delete obj._id;
+//   return obj;
+// });
 
-module.exports = mongoose.model("DgApp", DgAppSchema);
+// module.exports = mongoose.model("DgApp", DgAppSchema);
--- routers/dgApp.js
+++ routers/dgApp.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const {
-  createDgApp,
-  getDgApp,
-  getDgAppById,
-  getOneDgApp,
-  updateDgApp,
-  deleteDgApp,
-} = require("../controllers/dgApp");
+// const express = require("express");
+// const {
+//   createDgApp,
+//   getDgApp,
+//   getDgAppById,
+//   getOneDgApp,
+//   updateDgApp,
+//   deleteDgApp,
+// } = require("../controllers/dgApp");
 
-const router = express.Router();
-router.post("/dgapp", createDgApp);
-router.get("/dgapp", getDgApp);
-router.get("/dgapp/:dgAppId", getOneDgApp);
-router.put("/dgapp/:dgAppId", updateDgApp);
-router.delete("/dgapp/:dgAppId", deleteDgApp);
+// const router = express.Router();
+// router.post("/dgapp", createDgApp);
+// router.get("/dgapp", getDgApp);
+// router.get("/dgapp/:dgAppId", getOneDgApp);
+// router.put("/dgapp/:dgAppId", updateDgApp);
+// router.delete("/dgapp/:dgAppId", deleteDgApp);
 
-router.param("dgAppId", getDgAppById);
+// router.param("dgAppId", getDgAppById);
 
-module.exports = router;
+// module.exports = router;
