f48cb62aaf370583cdc9aec50d853a1cdd838b42
BAKCHA Sohaib Arbi <51241996+SohaibArbiBakcha@users.noreply.github.com>
2021-11-28 18:07:29 +0100

    comment delete and update

--- routers/travelinfo.js
+++ routers/travelinfo.js
@@ -4,16 +4,16 @@ const {
   getTravelinfo,
   travelinfoById,
   getOneTravelinfo,
-  updateTravelinfo,
-  deleteTravelinfo,
+  // updateTravelinfo,
+  // deleteTravelinfo,
 } = require("../controllers/travelinfo");
 
 const router = express.Router();
 router.post("/travelinfo", createTravelinfo);
 router.get("/travelinfo", getTravelinfo);
 router.get("/travelinfo/:travelinfoId", getOneTravelinfo);
-router.put("/travelinfo/:travelinfoId", updateTravelinfo);
-router.delete("/travelinfo/:travelinfoId", deleteTravelinfo);
+// router.put("/travelinfo/:travelinfoId", updateTravelinfo);
+// router.delete("/travelinfo/:travelinfoId", deleteTravelinfo);
 
 router.param("travelinfoId", travelinfoById);
 
