285d6998d31041e1581222397f952b937e962ce3
BAKCHA Sohaib Arbi <51241996+SohaibArbiBakcha@users.noreply.github.com>
2022-01-03 09:45:47 +0100

    modify refresh token

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "1006101348022-rto06it2mr0dt097028fttg8b7kne7sm.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04Aus2gyCwKQ2CgYIARAAGAQSNgF-L9IribUMBVlHA5f7_ZWefpx3t9Mw2own_6oR5nJF0adch3-a_pA8nsAtUn94xvjTCtDK7g",
+    "1//049MiG4h5V786CgYIARAAGAQSNgF-L9Ir9BinT2iKbfozUyb7Hb2-AGu97AmVMAVtrIlKKm9feJ1wziS1eBwWXbcMU70A14GV4Q",
 };
