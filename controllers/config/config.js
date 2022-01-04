cc708c4ef703d1b0bac0ab3eab96b619117ffa8e
AmineChegdali <79927617+AmineChegdali@users.noreply.github.com>
2022-01-04 21:11:19 +0100

    new refresh token

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "1006101348022-rto06it2mr0dt097028fttg8b7kne7sm.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//049MiG4h5V786CgYIARAAGAQSNgF-L9Ir9BinT2iKbfozUyb7Hb2-AGu97AmVMAVtrIlKKm9feJ1wziS1eBwWXbcMU70A14GV4Q",
+    "1//04E8n2cb1YCcACgYIARAAGAQSNgF-L9IrLeJCVNfKTjXZ1-LdQ3Vt8Hp4WyK9NJ8KJex6vcarZc7slbb8yYaGGxyEyhRLol27kQ",
 };
