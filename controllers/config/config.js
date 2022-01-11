582b24c404088d74f72810d7fa93440ec4d5702e
AmineChegdali <chegdali.amine@gmail.com>
2022-01-11 17:41:07 +0100

    restrictions

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04Erbep6OjJhBCgYIARAAGAQSNgF-L9IrYCmoAvqP679ZrXa0HAtjNF1GlpPxnvYx0XBbAQwjr-BY23Zh_aX6g1XhxBL02djF3A",
+  "1//04k-7t9xVmyLPCgYIARAAGAQSNgF-L9IrEtKlfHyN4fMPJQ9HP5pBE8dhxMIMx7NkByghkCPJcYFMJxMFmSxlxEt7U-CTpk98Nw",
 };
