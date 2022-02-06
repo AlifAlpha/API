6c54e1533380bdad2dd904e89f0b3a3e6fa0548b
AmineChegdali <79927617+AmineChegdali@users.noreply.github.com>
2022-02-06 17:21:00 +0100

    6febmail

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04x9VoiWYa-vHCgYIARAAGAQSNgF-L9IrR3H40o49rBu8PRnPiFM-khoG6yA_mSM31G4ezUS-iQWqwGaIKQLs9sEGRc9mU-tTOw",
+    "1//041TR6FZQHJ9dCgYIARAAGAQSNgF-L9IrfLsHkIZUEqPtV2O6KFTHE_0wSy_SooJtxxfbhy873G1dIyZjiNmC0EMCA8N6tKz6GQ",
 };
