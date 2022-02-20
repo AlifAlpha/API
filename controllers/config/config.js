f9dcfd4db24a0c08e9e84bc8611af56701fc61c5
BAKCHA Sohaib Arbi <51241996+SohaibArbiBakcha@users.noreply.github.com>
2022-02-20 16:11:37 +0100

    reset refresh token

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04HX82AknCjzYCgYIARAAGAQSNgF-L9Ir1SPPaOUhznh6STg0K3d8mL67BrN4YZxFXv1GRXj_DpH6LH9UTww0YXILjIFd_BYncg",
+     "1//047HyvWEHaKI7CgYIARAAGAQSNgF-L9IrLQ2kXacBXVN-i408CZBJ4d7sE6EFviAUM2-QfdaStiTMQgGYjhaeGt3JV9IPSgO-_g",
 };
