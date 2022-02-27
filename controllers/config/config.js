7f84d3839846a020362bdb71cc370c9d68152e1c
AmineChegdali <79927617+AmineChegdali@users.noreply.github.com>
2022-02-27 15:30:40 +0100

    27february3h30

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-     "1//047HyvWEHaKI7CgYIARAAGAQSNgF-L9IrLQ2kXacBXVN-i408CZBJ4d7sE6EFviAUM2-QfdaStiTMQgGYjhaeGt3JV9IPSgO-_g",
+  "1//04RVvtpcD4qYICgYIARAAGAQSNgF-L9IrE_d-3pdz4t0FyluzmArcbcGCCPiOubvi2_yWmQ6UQ1WFZ208Bz_VBzUTulzYQqmrzQ",
 };
