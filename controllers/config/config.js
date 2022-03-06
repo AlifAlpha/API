e45f708173ac03400245af9bc92183fe4d47f6b2
AmineChegdali <chegdali.amine@gmail.com>
2022-03-06 14:19:20 +0100

    mail google

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-  "1//04RVvtpcD4qYICgYIARAAGAQSNgF-L9IrE_d-3pdz4t0FyluzmArcbcGCCPiOubvi2_yWmQ6UQ1WFZ208Bz_VBzUTulzYQqmrzQ",
+  "1//04juO_RHwhOOYCgYIARAAGAQSNgF-L9Ir8S4XiuOMUJTvTSXZ0i91tnj3CMejoV3f8yQ8JORXNdC169-Ehi2uorUhGbjE6c8ktw",
 };
