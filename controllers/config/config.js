cf367d7c285758573e8803f6327c82b9649c84d9
AmineChegdali <79927617+AmineChegdali@users.noreply.github.com>
2022-03-25 14:05:22 +0100

    refresh 25march

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04wen0FUfI-bXCgYIARAAGAQSNgF-L9Irc3RRvQJR0AOooDCnS2hXJcm38cke2-7cB7SHzMlk9tav-wCUn2qzI-lKd5ZDCtgcaA",
+    "1//04H3j1AdPWoSdCgYIARAAGAQSNgF-L9IraGEAcKaSeGE2R7qOOSedoSFo_YhJbv_8vYHxxsOwMyF1G8D-iFQCXKmA7Q4t5XbOUg",
 };
