27125d706bc56a6b68d9ca22e8a35ccea8e165d6
BAKCHA Sohaib Arbi <51241996+SohaibArbiBakcha@users.noreply.github.com>
2022-04-01 14:21:53 +0000

    update the refrush token

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04H3j1AdPWoSdCgYIARAAGAQSNgF-L9IraGEAcKaSeGE2R7qOOSedoSFo_YhJbv_8vYHxxsOwMyF1G8D-iFQCXKmA7Q4t5XbOUg",
+    "1//04pfWpFYed-HMCgYIARAAGAQSNgF-L9Ir_CmUR3-QaNBFgkkYEGiq5gR9rqOYTKlivQ64oOnWmYwsnja_X34NzYSw97v6p4vs_w",
 };
