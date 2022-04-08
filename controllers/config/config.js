1f2e55390515f6c0ff22e0192af340986e8fdc14
AmineChegdali <79927617+AmineChegdali@users.noreply.github.com>
2022-04-08 23:45:52 +0200

    8avrmail

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04pfWpFYed-HMCgYIARAAGAQSNgF-L9Ir_CmUR3-QaNBFgkkYEGiq5gR9rqOYTKlivQ64oOnWmYwsnja_X34NzYSw97v6p4vs_w",
+  "1//04wLnLxsdys0sCgYIARAAGAQSNgF-L9IrWZTIuHurP_FRHpN3VSwL6DmuDZj3Q24zsvuDoaYxGYQxSloM-4G_KOLYsCSJLYmZaA",
 };
