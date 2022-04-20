2c705691057595c8ed677030c16b8fa49d79ed2a
AmineChegdali <79927617+AmineChegdali@users.noreply.github.com>
2022-04-20 11:55:27 +0200

    20avril email

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04wLnLxsdys0sCgYIARAAGAQSNgF-L9IrWZTIuHurP_FRHpN3VSwL6DmuDZj3Q24zsvuDoaYxGYQxSloM-4G_KOLYsCSJLYmZaA",
+    "1//04Nuet7ZI4WeOCgYIARAAGAQSNgF-L9IrMl6hVCk9Yor5YGKtEBrCcYRb-H32P-ldQPdLcKDJVCyWLR2dj8mgoyJ3Wh7bi5E55A",
 };
