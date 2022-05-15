83990e5e3aa133b52fcf57f4e2435024680a7d90
AmineChegdali <chegdali.amine@gmail.com>
2022-05-15 17:38:45 +0100

    mail 15 mai

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04Nuet7ZI4WeOCgYIARAAGAQSNgF-L9IrMl6hVCk9Yor5YGKtEBrCcYRb-H32P-ldQPdLcKDJVCyWLR2dj8mgoyJ3Wh7bi5E55A",
+    "1//04GcKcHdZkbChCgYIARAAGAQSNgF-L9Irao2yL7kR5ICvNku-JwXclEzhc6l3nRsev4050u7SfLGbRv1i4C8xY-3o_8eO6O6UHQ",
 };
