8528bcca34afcff2cd26cb9ed664997b19c17699
AmineChegdali <79927617+AmineChegdali@users.noreply.github.com>
2022-07-26 13:05:10 +0100

    refresh token

--- controllers/config/config.js
+++ controllers/config/config.js
@@ -4,5 +4,5 @@ module.exports = {
     "427783993539-p565j0f57num5sooe8aonucbtchid8hn.apps.googleusercontent.com",
   clientSecret: "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",
   refreshToken:
-    "1//04oBV2pNuLYIBCgYIARAAGAQSNgF-L9Irbf5EoKR2LAQNt5RDJKr2bURs2EZbS7csNh_LtC3Y2hqccO3nE1XKiFh7AhBC1-OiQw",
+    "ya29.A0AVA9y1s4FtcKVxYBVFLDNQfoYwWqOWKd3eanomhlTCAQqH1alFvnxduGvsotqVtCoSiTawFIRDDqqcorgWR0USV8Q1DFWz3VeNIf7tF4ggK0Lro8eMcAi7fD66EC_aFEMT-yZnHPgGElyl0ennDO9fPvhp-pYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4ZUViTzBFRWd6QUYyNXFmRV84SnpOZw0163",
 };
